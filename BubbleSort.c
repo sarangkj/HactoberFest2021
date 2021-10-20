c0fd73b4e06b7dd5ab17b4d1a744c56306dac857
sarangkj <56405345+sarangkj@users.noreply.github.com>
2021-10-20 20:46:27 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -14,28 +14,42 @@ int main()
   for (c = 0; c < n; c++)
     scanf("%ld", &array[c]);
 
-  bubble_sort(array, n);
+   bubbleSort(array, n);
 
   printf("Sorted list in ascending order:\n");
+   printArray(array, n);
 
-  for (c = 0; c < n; c++)
-     printf("%ld\n", array[c]);
+ void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)     
+ 
+       // Last i elements are already in place  
+       for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
   return 0;
+  
+  
 }
 
-void bubble_sort(long list[], long n)
-{
-  long c, d, t;
-
-  for (c = 0 ; c < n - 1; c++) {
-    for (d = 0 ; d < n - c - 1; d++) {
-      if (list[d] > list[d+1]) {
-        /* Swapping */
-        t         = list[d];
-        list[d]   = list[d+1];
-        list[d+1] = t;
-      }
-    }
-  }
-}
+ 
